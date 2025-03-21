532fdf201850e9a9f6c2fd682d71313f25095629
hyemee <svsvdvdv@naver.com>
2025-03-21 11:29:52 +0900

    폼 로그인 인증 with JPA

--- src/main/java/com/example/zzyzzy/spring_security/config/SecurityConfig.java
+++ src/main/java/com/example/zzyzzy/spring_security/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.zzyzzy.spring_security.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,20 +18,30 @@ import javax.servlet.http.HttpServletResponse;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final UserDetailsService userDetailsService;
 
     // SecurityFilterChain : 스프링 시큐리티에서 적용된 보안 규칙들을 필터로 구현해 둔 것
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .csrf().disable() // CSRF 필터 끔
+            .userDetailsService(userDetailsService) // userDetailsService 설정
             .authorizeRequests() // URL 기반 인가 설정
                 .antMatchers("/user/**").hasRole("USER") // USER 권한 사용자만 접근 가능
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/logout").authenticated() // 인증 받은 사용자만 접근 가능
                 .antMatchers("/**").permitAll() // 인증/인가 여부와 상관없이 접근 가능
                 .and()
-            .formLogin()  // form login 인증 사용 -- 여기 수정
+            .formLogin()  // form login 인증 사용
+                .loginProcessingUrl("/api/login") // 로그인 처리 URL
+                .usernameParameter("username") // 아이디 매개변수 지정
+                .passwordParameter("password") // 비밀번호 매개변수 지정
+                .defaultSuccessUrl("/") // 로그인 성공 시 리다이렉트 URL
+                .failureUrl("/login?error=true") // 로그인 실패 시 리다이렉트 URL
+                .permitAll()
                 .and()
             .logout()// 로그아웃 설정
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout")) // 로그아웃 URL 지정
@@ -47,21 +58,6 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {    // 비밀번호 암호화에 사용할 인코더
-        UserDetails user = User.builder()
-                .username("user")
-                .password(passwordEncoder.encode("password"))
-                .roles("USER")
-                .build();
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder.encode("password"))
-                //.roles("ADMIN", "USER") // ADMIN과 USER 권한 가짐
-                .roles("ADMIN") // ADMIN 권한 가짐
-                .build();
 
-        return new InMemoryUserDetailsManager(user, admin);
-    }
 
 }
